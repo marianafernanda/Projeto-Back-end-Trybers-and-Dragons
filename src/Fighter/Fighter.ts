d125cfdc522642885f3acd8ac4e65804f8a44d77
Mariana Fernanda <ariana.fernanda@icloud.com>
2022-12-16 17:22:25 -0300

    Requisitos 8 e 9

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,13 +1,14 @@
 import Energy from '../Energy';
+import SimpleFighter from './SimpleFighter';
 
-export default interface Fighter {
+export default interface Fighter extends SimpleFighter{
   lifePoints: number;
   strength: number;
   defense: number;
   energy?: Energy;
 
-  attack(enemy: Fighter): void;
-  special?(enemy: Fighter): void;
+  attack(enemy: Fighter | SimpleFighter): void;
+  special?(enemy: Fighter | SimpleFighter): void;
   levelUp(): void;
   receiveDamage(attackPoints: number): number;
 }
