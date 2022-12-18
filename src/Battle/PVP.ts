87835ecfa1e3f3916b41e5ad1175f1e6c72f00b4
Mariana Fernanda <ariana.fernanda@icloud.com>
2022-12-18 14:03:59 -0300

    Requisito 12

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -1,5 +1,13 @@
-// import Battle from './Battle';
+import Battle from './Battle';
+import Character from '../Character';
 
-// export default class PVP extends Battle {
+export default class PVP extends Battle {
+  player1: Character;
+  player2: Character;
 
-// }
+  constructor(player1: Character, player2: Character) {
+    super(player1);
+    this.player1 = player1;
+    this.player2 = player2;
+  }
+}
